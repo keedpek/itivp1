96ac19e45a84b129587b626df7eb373ba58cb737
keedpek <zaharv20004@gmail.com>
2025-02-13 01:55:13 +0300

    feat: burger menu

---
+++ scripts/burgerMenu.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuBtn = document.querySelector('.burger-menu')
+  const menu = document.querySelector('.mobile-menu')
+  
+  menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle("active");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+    console.log(1);
+    
+  })
+})
