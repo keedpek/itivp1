143eee42194ab07158e2c2890d0be2c40c5badb2
keedpek <zaharv20004@gmail.com>
2025-03-22 17:40:49 +0300

    fix: new story placement

--- scripts/newStoryTimer.js
+++ scripts/newStoryTimer.js
@@ -1,12 +1,12 @@
 const container = document.querySelector('.stories-container')
 const newStoryInnerHtml = `
-    <img src="../images/stories/the-best-time-to-visit-hawaii.webp" alt="story1">
-    <div class="meta">
-      <h4>Гаваи, США</h4>
-      <span>27 фев, 2024 • ⏲ 8 мин</span>
-    </div>
-    <h3 class="story-title">Лучшее Время для Посещения Гаваи</h3>
-    <p class="short-story">Меня всегда интересовала духовность, поэтому я решил совершить годичное путешествие в Индию, чтобы изучить различные религиозные практики и традиции.</p>
+    <img src="../images/stories/how-to-move-around-athens.webp" alt="story2">
+      <div class="meta">
+        <h4>Афины, Греция</h4>
+        <span>27 фев, 2024 • ⏲ 8 мин</span>
+      </div>
+    <h3 class="story-title">Как передвигаться по Афинам</h3>
+    <p class="short-story">Я только что окончил колледж и решил отправиться в шестимесячное одиночное путешествие по Европе, прежде чем начать свою карьеру. Я путешествовал с рюкзаком по 15 разным странам, останавливаясь в хостелах и встречаясь с людьми.</p>
     <a class="full-post-link" href="">Читать весь пост</a>
 `
 const timer = 10000
@@ -20,8 +20,6 @@ function createNewStory(innerHtml) {
   const newStory = document.createElement('div')
   newStory.classList.add('story-component')
   newStory.innerHTML = innerHtml
-  container.appendChild(newStory)
-  console.log(newStory);
-  
+  container.prepend(newStory)
   alert('Новая история уже доступна!!!')
 }
